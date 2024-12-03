ea8511e7ffd91e53e4101dca6eb3efb68b96c0ce
sergg57 <sergg57@list.ru>
2024-12-03 23:08:25 +0300

    Add files via upload

---
+++ DZ_13_2.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+from pyexpat.errors import messages
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+import asyncio
+
+api = "8013716336:AAEdVmWV4Lw3Va2wdLEfb-2F1KkWLtCxNPA"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    print(f'Привет! Я бот помогающий твоему заданию')
+    #print(f'Запущена команда {message.text}')
+    # await message.reply('Привет! Я бот помогающий твоему заданию')
+
+@dp.message_handler(content_types=['text'])
+async def all_messages(message):
+    print(f'Введите команду /start, чтобы начать общение!')
+    #print(f'Введите команду /start, чтобы начать общение! {message.text}')
+
+# @dp.message_handler()
+# async def all_messages(message: types.Message):
+#     print(f'Введите команду /start, чтобы начать общение {message.text}')
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
